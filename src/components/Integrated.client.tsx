816e6ff59e129a8f5263bbe1d24c80dabb17ee8e
Farah <farah.zaqout@gmail.com>
2024-05-22 17:20:32 +0300

    Update Integrated.client.tsx

--- src/components/Integrated.client.tsx
+++ src/components/Integrated.client.tsx
@@ -3,13 +3,68 @@ import React from 'react';
 import { frappeObject } from './object';
 
 interface Field {
+    doctype: string;
     name: string;
-    label?: string;
-    required: boolean;
-    readOnly: boolean;
+    creation: string;
+    modified: string;
+    modified_by: string;
+    owner: string;
+    docstatus: number;
+    parent: string;
+    parentfield: string;
+    parenttype: string;
+    idx: number;
+    fieldname: string;
+    label: string;
+    fieldtype: 'Text' | 'Number' | 'Email' | 'Date' | 'Select' | 'Checkbox' | 'TextArea' | 'Section Break';
     options?: string;
-    fieldtype: 'Text' | 'Number' | 'Email' | 'Date' | 'Select' | 'Checkbox' | 'TextArea';
+    search_index: number;
+    show_dashboard: number;
+    hidden: number;
+    set_only_once: number;
+    allow_in_quick_entry: number;
+    print_hide: number;
+    report_hide: number;
+    reqd: number;
+    bold: number;
+    in_global_search: number;
+    collapsible: number;
+    unique: number;
+    no_copy: number;
+    allow_on_submit: number;
+    show_preview_popup: number;
+    permlevel: number;
+    ignore_user_permissions: number;
+    columns: number;
+    in_list_view: number;
+    fetch_if_empty: number;
+    in_filter: number;
+    remember_last_selected_value: number;
+    ignore_xss_filter: number;
+    print_hide_if_no_value: number;
+    allow_bulk_edit: number;
+    in_standard_filter: number;
+    in_preview: number;
+    read_only: number;
+    length: number;
+    translatable: number;
+    hide_border: number;
+    hide_days: number;
+    hide_seconds: number;
+    non_negative: number;
+    is_virtual: number;
+    sort_options: number;
+    fields: Field[];
+    permissions: any[];  // Type as needed
+    actions: any[];  // Type as needed
+    links: any[];  // Type as needed
+    states: any[];  // Type as needed
+    search_fields?: string;  // Nullable if not always present
+    is_custom_field?: boolean;  // Nullable if not always present
+    linked_document_type?: string;  // Nullable if not always present
+    default?: string;  // Optional for fields like 'Select'
 }
+
 
 interface Document {
     doctype: string;
@@ -33,31 +88,33 @@ interface EnhancedFieldConfig extends Field {
 }
 
 const DefaultComponents: CustomComponents = {
-    Text: ({ config }) => (
-        <input type="text" name={config.name} placeholder={config.label} required={config.required} readOnly={config.readOnly} onChange={config.onChange}  className="input input-bordered w-full max-w-xs" />
-    ),
+    Text: ({ config }) => {
+        return (
+            <input type="text" name={config.name} placeholder={config.label} required={Boolean(config.reqd)} readOnly={Boolean(config.read_only)} onChange={config.onChange}  className="input input-bordered w-full max-w-xs" />
+        )
+    },
     Number: ({ config }) => (
-        <input onChange={config.onChange} type="number" name={config.name} placeholder={config.label} required={config.required} readOnly={config.readOnly} className="input input-bordered w-full max-w-xs" />
+        <input onChange={config.onChange} type="number" name={config.name} placeholder={config.label} required={Boolean(config.reqd)} readOnly={Boolean(config.read_only)} className="input input-bordered w-full max-w-xs" />
     ),
     Email: ({ config }) => (
-        <input onChange={config.onChange} type="email" name={config.name} placeholder={config.label} required={config.required} readOnly={config.readOnly} className="input input-bordered w-full max-w-xs" />
+        <input onChange={config.onChange} type="email" name={config.name} placeholder={config.label} required={Boolean(config.reqd)} readOnly={Boolean(config.read_only)} className="input input-bordered w-full max-w-xs" />
     ),
     Date: ({ config }) => (
-        <input onChange={config.onChange} type="date" name={config.name} required={config.required} readOnly={config.readOnly} className="input input-bordered w-full max-w-xs" />
+        <input onChange={config.onChange} type="date" name={config.name} required={Boolean(config.reqd)} readOnly={Boolean(config.read_only)} className="input input-bordered w-full max-w-xs" />
     ),
     Select: ({ config }) => (
-        <select onChange={config.onChange} name={config.name} required={config.required} className="select select-bordered w-full max-w-xs">
+        <select onChange={config.onChange} name={config.name} required={Boolean(config.reqd)} className="select select-bordered w-full max-w-xs">
             {config.options?.split('\n').map((option, index) => <option key={index} value={option.trim()}>{option.trim()}</option>)}
         </select>
     ),
     Checkbox: ({ config }) => (
         <label className="label cursor-pointer">
-            <input onChange={config.onChange} type="checkbox" name={config.name} checked={config.required} readOnly={config.readOnly} className="checkbox checkbox-primary" />
+            <input onChange={config.onChange} type="checkbox" name={config.name} checked={Boolean(config.reqd)} readOnly={Boolean(config.read_only)} className="checkbox checkbox-primary" />
             {config.label}
         </label>
     ),
     TextArea: ({ config }) => (
-        <textarea onChange={config.onChange} name={config.name} placeholder={config.label} required={config.required} readOnly={config.readOnly} className="textarea textarea-bordered h-24 w-full" />
+        <textarea onChange={config.onChange} name={config.name} placeholder={config.label} required={Boolean(config.reqd)} readOnly={Boolean(config.read_only)} className="textarea textarea-bordered h-24 w-full" />
     )
 };
 
@@ -82,11 +139,10 @@ export function createFieldConfig(formFields: FrappeObject): { [key: string]: En
 
     formFields.docs.forEach(doc => {
         doc.fields.forEach(field => {
-            console.log({required: field.required})
             fieldConfigurations[field.name] = {
                 ...field,
-                required: field.required,
-                readOnly: field.readOnly,
+                reqd: field.reqd,
+                read_only: field.read_only,
                 options: field.options
             };
             fieldConfigurations[field.name] = enhanceFieldConfig(fieldConfigurations[field.name]);
